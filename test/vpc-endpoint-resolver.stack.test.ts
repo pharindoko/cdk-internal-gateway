1b6d443c836d4b0d408b4517d1a89672df6d40f6
uid10804 <florian.fuss@vitesco.com>
2022-10-28 21:25:29 +0200

    style(endpoint-resolver): remove unused reference

--- test/vpc-endpoint-resolver.stack.test.ts
+++ test/vpc-endpoint-resolver.stack.test.ts
@@ -1,7 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { ResourceQueryResolverStack } from '../src/custom-resource-resolver.stack';
-import { LocalNSLookupResolverStack } from '../src/local-nslookup-resolver.stack';
 
 test('VPC Endpoint Custom Resource Query Resolver Stack provider', () => {
   const app = new cdk.App();
