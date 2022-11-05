9e75659f1673159ec3e831c3c238deb2d2c67fcd
uid10804 <florian.fuss@vitesco.com>
2022-11-06 00:04:34 +0100

    chore: update projen description and keywords

--- .projenrc.js
+++ .projenrc.js
@@ -8,9 +8,9 @@ const project = new awscdk.AwsCdkConstructLibrary({
   name: 'cdk-internal-gateway',
   repositoryUrl: 'https://github.com/pharindoko/cdk-internal-gateway.git',
   license: 'Apache-2.0',
-  description: 'CDK construct to create an internal aws api-gateway to ease the creation of internal websites, apis or applications',
+  description: 'CDK construct to create to create internal serverless applications.',
   sampleCode: false,
-  keywords: ['cdk', 'apigateway', 'internal', 'gateway', 'vpc', 'network', 'api', 'website', 'application'],
+  keywords: ['cdk', 'apigateway', 'internal', 'private', 'gateway', 'vpc', 'network', 'api', 'website', 'application', 'company', 'enterprise'],
   gitignore: [
     'cdk.out',
     'cdk.context.json',
